90e780532849705763da65e8cc8887cb4a1c57af
Ivan Carvalho <ivancarvalho@gatech.edu>
2026-01-10 16:20:50 -0500

    Homebrew: update formula to use pre-built binaries for mac

--- brew/celq.rb
+++ brew/celq.rb
@@ -1,14 +1,33 @@
 class Celq < Formula
   desc "A Common Expression Language (CEL) CLI Tool"
   homepage "https://github.com/IvanIsCoding/celq"
-  url "https://github.com/IvanIsCoding/celq/archive/refs/tags/{{CELQ_VERSION}}.tar.gz"
-  sha256 "{{CELQ_SHA256}}"
+  version "{{CELQ_VERSION}}"
   license "MIT OR Apache-2.0"
 
-  depends_on "rust" => :build
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/IvanIsCoding/celq/releases/download/v{{CELQ_VERSION}}/celq-aarch64-apple-darwin.tar.gz"
+      sha256 "{{CELQ_SHA256_ARM64}}"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/IvanIsCoding/celq/releases/download/v{{CELQ_VERSION}}/celq-x86_64-apple-darwin.tar.gz"
+      sha256 "{{CELQ_SHA256_X86_64}}"
+    end
+  else
+    # Fallback to source for Linux
+    url "https://github.com/IvanIsCoding/celq/archive/refs/tags/{{CELQ_VERSION}}.tar.gz"
+    sha256 "{{CELQ_SHA256_SOURCE}}"
+    depends_on "rust" => :build
+  end
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    if OS.mac?
+      # Install pre-built binary
+      bin.install "celq"
+    else
+      # Build from source
+      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    end
   end
 
   test do
