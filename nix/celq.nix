4adcbbf6e5b7caadc02ac2171f5ed70e06a1e1df
Ivan Carvalho <ivancarvalho@gatech.edu>
2026-03-28 17:35:11 -0400

    Bump Nix hashes for 0.4.0

--- nix/celq.nix
+++ nix/celq.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "celq";
-  version = "0.3.4";
+  version = "0.4.0";
 
   # Fetch from crates.io
   src = fetchCrate {
     pname = finalAttrs.pname;
     version = finalAttrs.version;
-    sha256 = "sha256-nKc7TBu+UvrSZMPd+BOExcDn4RPvQVpq3pcNSeNLFhs=";
+    sha256 = "sha256-54lMkgGch7GwpuOmH2y67VNLTC1hMbhkqjLf94DoekA=";
   };
 
-  cargoHash = "sha256-7iJ+D5/7O2XPIT5ocidvngd7YqHFhBxUn22SNJ4u2G0=";
+  cargoHash = "sha256-cUpf9/h95MD36xQaRPwg2Fz9lQVxK+q8ftP4B4gJEOc=";
 
   nativeInstallCheckInputs = [
     versionCheckHook
